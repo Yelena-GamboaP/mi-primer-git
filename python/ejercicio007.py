00aed776479cc0d71a08a738ad186a4763c3305f
Yelena Gamboa <yagamboa_97.fca@hotmail.com>
2021-06-07 18:25:05 -0500

    calculo de un triangulo

--- python/ejercicio007.py
+++ python/ejercicio007.py
@@ -1,1 +1,6 @@
 #github
+base  = float (input("ingresa la base del triangulo: \t"))  
+altura  = float (input("ingresa la altura del triangulo: \t")) 
+
+print (f"El resultado del area del triangulo es {(base * altura)/2}")
+print (f"El resultado del perimetro del triangulo es {base * 3}")

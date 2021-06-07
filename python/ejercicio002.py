23f084dcc721912b7847cc959ad1b2c9d95e9e21
Yelena Gamboa <yagamboa_97.fca@hotmail.com>
2021-06-07 17:37:40 -0500

    mi primer commit

---
+++ python/ejercicio002.py
@@ -0,0 +1,14 @@
+"""
+calcular el area y el perimetro de un cuadrado
+"""
+#ingreso de datos
+lado= float (input("ingresar medida del lado del cuadrado: "))
+
+#proceso
+perimetro=lado*4
+area=lado*lado
+
+#resultado
+print(f'El perimetro del cuadrado es: {perimetro}')
+print(f'el area del cuadrado es: {area}')
+
---
+++ python/ejercicio006.py
@@ -0,0 +1,8 @@
+edad = int (imput ("Ingrese su edad: "))
+
+if (edad >64):
+    print("TERCERA EDAD")
+elif (edad > 17):
+    print("MAYOR DE EDAD")
+else:
+    print("menor de edad")
